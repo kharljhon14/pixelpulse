8c6b7f72493ff08d6c04b8bf2a81e0e6bfd2c9ba
kharle14 <enriquezkharl14@gmail.com>
2024-05-14 20:12:52 +0800

    [Feat] Card component

---
+++ src/ts/web-components/card.ts
@@ -0,0 +1,13 @@
+export class Card extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = ``;
+    }
+  }
+}
