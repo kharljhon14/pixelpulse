5df8fb9de2cdb4377aa74b7a61285fab48ba4784
kharle14 <enriquezkharl14@gmail.com>
2024-05-14 20:55:31 +0800

    [Feat] Card Component

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,5 @@
 import { Button } from './web-components/button';
+import { Card } from './web-components/card';
 import { Chip } from './web-components/chip';
 import { Header } from './web-components/header';
 import Sidebar from './web-components/sidebar';
@@ -7,3 +8,4 @@ if (!customElements.get('button-component')) customElements.define('button-compo
 if (!customElements.get('chip-component')) customElements.define('chip-component', Chip);
 if (!customElements.get('sidebar-component')) customElements.define('sidebar-component', Sidebar);
 if (!customElements.get('header-component')) customElements.define('header-component', Header);
+if (!customElements.get('card-component')) customElements.define('card-component', Card);
--- src/ts/web-components/card.ts
+++ src/ts/web-components/card.ts
@@ -1,3 +1,37 @@
+const styles = new CSSStyleSheet();
+
+styles.replaceSync(`
+:host{
+    display: inline-block;
+    position: relative;
+}
+
+.card{
+    border: 1px solid #000;
+    border-radius: 1.6rem;
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    background-color: #FFF;
+    transition: all .3s ease;
+    position: relative;
+    z-index: 2;
+}
+
+.card-bottom{
+    border: 1px solid #000;
+    position: absolute;
+    background-color: #000;
+    height: 100%;
+    width: 100%;
+    left: .2rem;
+    top: .2rem;
+    border-radius: 1.6rem;
+    transition: all .3s ease;
+}
+
+`);
+
 export class Card extends HTMLElement {
   constructor() {
     super();
@@ -7,7 +41,17 @@ export class Card extends HTMLElement {
 
   connectedCallback() {
     if (this.shadowRoot) {
-      this.shadowRoot.innerHTML = ``;
+      this.shadowRoot.adoptedStyleSheets = [styles];
+      this.shadowRoot.innerHTML = `
+        <div part="content" name="content" class="card">
+            <slot></slot>
+        </div>
+        <div class="card-bottom"></div>
+      `;
+
+      const card = this.shadowRoot.querySelector('.card') as HTMLElement;
+
+      card.style.backgroundColor = this.getAttribute('color') ?? '';
     }
   }
 }
